6268c299a05b635acff50d944209a7e8a6d08d99
Karlstad <oeyvind_karlstad@yahoo.no>
2016-08-29 08:29:11 +0200

    Allocate done

---
+++ Oving1/allocate.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//This will allocate memory until there are no more memory to give and the computer freezes
+void allocate(int value){
+	int *ptr = NULL;
+	ptr = malloc(100000 * sizeof(int));
+ 	*ptr = value;
+ 	printf("test of allocate memory: %i\n", ptr);
+} 
+
+int main(){
+	time_t t;
+
+	srand((unsigned) time(&t));
+	while(1){
+		allocate(rand());
+	}
+}
---
+++ Oving1/linked_list/linked_list.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "linked_list.h"
+
+list_t list_create()
+{
+
+}
+
+void list_delete(list_t list)
+{
+
+}
+
+void list_insert(list_t list, int index, int data)
+{
+
+}
+
+void list_append(list_t list, int data)
+{
+
+}
+
+void list_print(list_t list)
+{
+
+}
+
+long list_sum(list_t list)
+{
+
+}
+
+int list_get(list_t list, int index)
+{
+
+}
+
+int list_extract(list_t list, int index)
+{
+
+}
